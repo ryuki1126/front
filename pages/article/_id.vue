2cea39d60d0e730883d2b2451cef4be6b374ed56
ryuki1126 <uv.ryuki1126@gmail.com>
2022-01-27 15:13:28 +0900

    デザイン変更

--- pages/article/_id.vue
+++ pages/article/_id.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
-    <div>
-        <h1>
-          {{id}}
-        </h1>
-        <div class="mt-4" v-html="$md.render(content)"></div>
-    </div>
+    <v-card
+      class="my-3"
+      min-height=150vh
+    >
+        <v-card-title class="text-h5">
+          {{ posts.article_title }}
+        </v-card-title>
+        <v-card-subtitle>
+          作成日 : {{ posts.created_at }} , 更新日 : {{ posts.updated_at }}
+        </v-card-subtitle>
+        <div class="pl-8 mt-4" v-html="$md.render(content)"></div>
+    </v-card>
   </div>
 </template>
 
@@ -28,6 +34,10 @@ export default {
     axios.get(url)
       .then((res) => {
         console.log(res.data)
+        const created_at = res.data.created_at.substr(0, 10)
+        const updated_at = res.data.updated_at.substr(0, 10)
+        res.data.created_at = created_at
+        res.data.updated_at = updated_at
         this.content = res.data.article_content
         this.posts = res.data
       })
